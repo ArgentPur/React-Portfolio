26d8bad5399a65fdb08e408a53e31d9555e3dbb6
Zachary Lynn <zalynn25@hotmail.com>
2021-02-01 19:28:29 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my developer portfolio.
         </p>
+        <img src= "https://avatars.githubusercontent.com/u/68625109?s=460&u=18d6fc86f9c6140410da8e59d7bb1b23a7125546&v=4" alt="logo" />
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ArgentPur"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ArgentPur @ Github
         </a>
       </header>
+      <body>
+        <div class= "container">
+
+        </div>
+      </body>
     </div>
   );
 }
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
 export const Header =(props) => (
-    <div className= 'header'></div>
+    <div className= 'header'>
+        <header className="App-header">
+        <p>
+          Welcome to my developer portfolio.
+        </p>
+        <img src= "https://avatars.githubusercontent.com/u/68625109?s=460&u=18d6fc86f9c6140410da8e59d7bb1b23a7125546&v=4" alt="logo" />
+
+        <a
+          className="App-link"
+          href="https://github.com/ArgentPur"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          ArgentPur @ Github
+        </a>
+      </header>
+    </div>
 )
