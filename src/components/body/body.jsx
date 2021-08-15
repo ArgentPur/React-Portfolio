c8ce2ba8425e8d01011b8565d7de33dd94661596
Zachary Lynn <zalynn25@hotmail.com>
2021-08-14 21:37:53 -0400

    update

--- src/components/body/body.jsx
+++ src/components/body/body.jsx
@@ -4,18 +4,27 @@ import './body.styles.css'
 
 export const Body = (props) => (
     <div className ="body">
-        <a href= {allProjects[0].url}>{allProjects[0].name}</a><br/>        
-        <a href= {allProjects[1].url}>{allProjects[1].name}</a><br/>       
+        <div class= "project">
+        <a href= {allProjects[0].url}>{allProjects[0].name}</a><br/>
+        <p>{allProjects[0].description}</p>
+        </div>        
+        <div class= "project">
+        <a href= {allProjects[1].url}>{allProjects[1].name}</a><br/>
+        <p>{allProjects[1].description}</p>
+        </div>       
+        <div class= "project">
         <a href= {allProjects[2].url}>{allProjects[2].name}</a><br/>
-        <div>      
+        <p>{allProjects[2].description}</p>
+        </div>
+        <div class= "project">      
         <a href= {allProjects[3].url}>{allProjects[3].name}</a><br/>
         <p>{allProjects[3].description}</p>
         </div> 
-        <div>   
+        <div class= "project">   
         <a href= {allProjects[4].url}>{allProjects[4].name}</a><br/>
         <p>{allProjects[4].description}</p>
         </div>   
-        <div>
+        <div class= "project">
         <a href= {allProjects[5].url}>{allProjects[5].name}</a><br/>
         <p>{allProjects[5].description}</p>
         </div>
--- src/projects.js
+++ src/projects.js
@@ -4,22 +4,22 @@ export const allProjects =
     {
         "name": "NoteBook",
         "url": "https://notebook-argentpur.herokuapp.com/",
-        "description": ""
+        "description": "A simple note taking app deployed on Heroku"
     },
     {
         "name": "Profile",
         "url": "https://argentpur.github.io/Personal-Portfolio/",
-        "description": ""
+        "description": "My personal profile site, built using Boostrap 4"
     },
     {
         "name": "GIVING",
         "url": "https://github.com/ArgentPur/GIVING",
-        "description": ""
+        "description": "A collaborative group project, GIVING is a React app that uses a Mongo database and features passport authenitcation."
     },
     {
         "name": "Opus_1 - Music Resource Page",
         "url": "https://argentpur84.github.io/Opus1/",
-        "description": "Opus_1 is a resource page that I created for my students studying piano, and is my favorite example of how learning web development has helped in my carreer as an instructor..  "
+        "description": "Opus_1 is a resource page that I created for my students studying piano, and is my favorite example of how learning web development has helped in my carreer as an instructor."
     },
     {
         "name": "Python-Powered Password",
@@ -29,7 +29,7 @@ export const allProjects =
     {
         "name": "WeatherApp",
         "url": "https://argentpur.github.io/WeatherAPI/",
-        "description": "A simple application that makes an API request for weather information."
+        "description": "A simple application that makes an API request for weather information"
     }
 ]
 
