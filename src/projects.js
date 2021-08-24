1edfc747fef849e3405a070c802889e00986072c
Zachary Lynn <zalynn25@hotmail.com>
2021-08-24 14:58:35 -0400

    revised

--- src/projects.js
+++ src/projects.js
@@ -4,22 +4,22 @@ export const allProjects =
     {
         "name": "NoteBook",
         "url": "https://notebook-argentpur.herokuapp.com/",
-        "description": "A simple note taking app deployed on Heroku"
+        "description": "A simple note taking app that allows the user to create and delete notes"
     },
     {
         "name": "Profile",
         "url": "https://argentpur.github.io/Personal-Portfolio/",
-        "description": "My personal profile site, built using Boostrap 4"
-    },
-    {
-        "name": "GIVING",
-        "url": "https://github.com/ArgentPur/GIVING",
-        "description": "A collaborative group project, GIVING is a React app that uses a Mongo database and features passport authenitcation."
+        "description": "My profile site, built using Boostrap 4"
     },
     {
         "name": "Opus_1 - Music Resource Page",
         "url": "https://argentpur84.github.io/Opus1/",
         "description": "Opus_1 is a resource page that I created for my students studying piano, and is my favorite example of how learning web development has helped in my carreer as an instructor."
+    },
+    {
+        "name": "GIVING",
+        "url": "https://github.com/ArgentPur/GIVING",
+        "description": "A collaborative group project, GIVING is a React app that uses a Mongo database and features passport authenitcation."
     },
     {
         "name": "Python-Powered Password",
